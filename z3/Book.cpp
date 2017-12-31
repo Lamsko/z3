40d6c31ea7770b5a7137125203a9be7bc3da2dc9
Witold Mormul <w.mormul@gmail.com>
2017-12-31 13:27:51 +0100

    koniec 1 zadania

--- z3/Book.cpp
+++ z3/Book.cpp
@@ -4,32 +4,60 @@
 int Book::ileKonstruktorKopiujacy = 0;
 int Book::ileKonstruktorParametryczny = 0;
 int Book::ileKonstruktorPrzenoszacy = 0;
-int Book::ileOperatorKopiujacy = 0;
-int Book::ileOperatorPrzenoszacy = 0;
+
 
 Book::Book(string t, string a, int ls) : tytul(t), autor(a), liczbaStron(ls)
 {
 	ileKonstruktorParametryczny++;
 }
 
-Book::Book(const Book & b)
+Book::Book(const Book & b) : tytul(b.tytul), autor(b.autor), liczbaStron(b.liczbaStron)
 {
 	ileKonstruktorKopiujacy++;
 }
 
-Book::Book(Book && b)
+Book::Book(Book && b) : tytul(""), autor(""), liczbaStron(0)
 {
 	ileKonstruktorPrzenoszacy++;
+
+	tytul = b.tytul;
+	autor = b.autor;
+	liczbaStron = b.liczbaStron;
+
+	b.tytul = "";
+	b.autor = "";
+	b.liczbaStron = 0;	
 }
 
-Book & Book::operator=(const Book &)
+Book & Book::operator=(const Book & b)
 {
-	ileOperatorKopiujacy++;
+	ileKonstruktorKopiujacy++;
+
+	if (this != &b)
+	{
+		tytul = b.tytul;
+		autor = b.autor;
+		liczbaStron = b.liczbaStron;
+	}
+
+	return *this;
 }
 
-Book & Book::operator=(Book &&)
+Book & Book::operator=(Book && b)
 {
-	ileOperatorPrzenoszacy++;
+	ileKonstruktorPrzenoszacy++;
+	if (this != &b)
+	{
+		tytul = b.tytul;
+		autor = b.autor;
+		liczbaStron = b.liczbaStron;
+
+		b.tytul = "";
+		b.autor = "";
+		b.liczbaStron = 0;
+	}
+
+	return *this;
 }
 
 Book::~Book()
